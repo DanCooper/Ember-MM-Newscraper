fa79bd815d0a49a02e00f81cb2a0416ff7ec500d
Massimo Savazzi <m.savazzi@who.net>
2013-06-25 16:23:32 +0200

    send part

--- Addons/generic.EmberCore.GenresEditor/My Project/Resources.Designer.vb
+++ Addons/generic.EmberCore.GenresEditor/My Project/Resources.Designer.vb
@@ -39,7 +39,7 @@ Namespace My.Resources
         Friend ReadOnly Property ResourceManager() As Global.System.Resources.ResourceManager
             Get
                 If Object.ReferenceEquals(resourceMan, Nothing) Then
-                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("generic.Compatibility.NMT.Resources", GetType(Resources).Assembly)
+                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("generic.EmberCore.GenresEditor.Resources", GetType(Resources).Assembly)
                     resourceMan = temp
                 End If
                 Return resourceMan
@@ -63,9 +63,9 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property film() As System.Drawing.Bitmap
+        Friend ReadOnly Property GenresEditor() As System.Drawing.Bitmap
             Get
-                Dim obj As Object = ResourceManager.GetObject("film", resourceCulture)
+                Dim obj As Object = ResourceManager.GetObject("GenresEditor", resourceCulture)
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
@@ -73,19 +73,9 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend ReadOnly Property icon() As System.Drawing.Bitmap
+        Friend ReadOnly Property image() As System.Drawing.Bitmap
             Get
-                Dim obj As Object = ResourceManager.GetObject("icon", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property television() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("television", resourceCulture)
+                Dim obj As Object = ResourceManager.GetObject("image", resourceCulture)
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
