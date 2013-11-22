5d37040b0a16084b82f6ae0ba1cc9b2314a658b1
Dekker500 <Dekker@Crater.com>
2013-11-22 14:36:50 -0500

    EmberAPI_Test\Test_clsAPIHTTP
    Fixed potential memory leak in unit test class by properly implementing
    IDisposable

--- EmberAPI_Test/Test_clsAPIHTTP.vb
+++ EmberAPI_Test/Test_clsAPIHTTP.vb
@@ -25,7 +25,11 @@ Imports EmberAPI
 
 Namespace EmberTests
 
-    <TestClass()> Public Class Test_clsAPIHTTP
+    <TestClass()>
+    Public Class Test_clsAPIHTTP
+        Implements IDisposable
+
+
         Dim HTTP As HTTP = Nothing
         ''' <summary>
         ''' Common setup for all tests in this class. This will run before EVERY test case in this class
@@ -140,6 +144,39 @@ Namespace EmberTests
             'Assert
             Assert.IsFalse(result, "Data tested was: 'Nothing' and was expecting 'False', but received '{0}'", result)
         End Sub
+
+#Region "IDisposable Support"
+        Private disposedValue As Boolean ' To detect redundant calls
+
+        ' IDisposable
+        Protected Overridable Sub Dispose(disposing As Boolean)
+            If Not Me.disposedValue Then
+                If disposing Then
+                    ' dispose managed state (managed objects).
+                    If HTTP IsNot Nothing Then HTTP.Dispose()
+                End If
+
+                ' free unmanaged resources (unmanaged objects) and override Finalize() below.
+                ' set large fields to null.
+                HTTP = Nothing
+            End If
+            Me.disposedValue = True
+        End Sub
+
+        ' TODO: override Finalize() only if Dispose(ByVal disposing As Boolean) above has code to free unmanaged resources.
+        'Protected Overrides Sub Finalize()
+        '    ' Do not change this code.  Put cleanup code in Dispose(ByVal disposing As Boolean) above.
+        '    Dispose(False)
+        '    MyBase.Finalize()
+        'End Sub
+
+        ' This code added by Visual Basic to correctly implement the disposable pattern.
+        Public Sub Dispose() Implements IDisposable.Dispose
+            ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+            Dispose(True)
+            GC.SuppressFinalize(Me)
+        End Sub
+#End Region
 
     End Class
 End Namespace
