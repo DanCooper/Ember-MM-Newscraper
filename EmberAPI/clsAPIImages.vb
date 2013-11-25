c6619414c16f70ed799776c346cabf0a33f04292
Dekker500 <Dekker@Crater.com>
2013-11-25 10:36:22 -0500

    EmberAPI\clsAPIImages
    - Enhanced iDisposable implementation to remove potential memory leaks

--- EmberAPI/clsAPIImages.vb
+++ EmberAPI/clsAPIImages.vb
@@ -363,21 +363,7 @@ Public Class Images
         End Try
     End Sub
 
-    Public Sub Dispose() Implements IDisposable.Dispose
-		If Not IsNothing(_ms) Then
-			_ms.Flush()
-			_ms.Close()
-			_ms.Dispose()
-			_ms = Nothing
-		End If
-		If Not IsNothing(_image) Then
-			_image.Dispose()
-			_image = Nothing
-		End If
-		_isedit = False
-    End Sub
-
-	Public Sub FromFile(ByVal sPath As String)
+    Public Sub FromFile(ByVal sPath As String)
         'Debug.Print("FromFile/t{0}", sPath)
         If Not IsNothing(Me._ms) Then
             Me._ms.Dispose()
@@ -385,26 +371,26 @@ Public Class Images
         If Not IsNothing(Me._image) Then
             Me._image.Dispose()
         End If
-		If Not String.IsNullOrEmpty(sPath) AndAlso File.Exists(sPath) Then
-			Try
-				Me._ms = New MemoryStream()
-				Using fsImage As New FileStream(sPath, FileMode.Open, FileAccess.Read)
-					Dim StreamBuffer(Convert.ToInt32(fsImage.Length - 1)) As Byte
+        If Not String.IsNullOrEmpty(sPath) AndAlso File.Exists(sPath) Then
+            Try
+                Me._ms = New MemoryStream()
+                Using fsImage As New FileStream(sPath, FileMode.Open, FileAccess.Read)
+                    Dim StreamBuffer(Convert.ToInt32(fsImage.Length - 1)) As Byte
 
-					fsImage.Read(StreamBuffer, 0, StreamBuffer.Length)
-					Me._ms.Write(StreamBuffer, 0, StreamBuffer.Length)
+                    fsImage.Read(StreamBuffer, 0, StreamBuffer.Length)
+                    Me._ms.Write(StreamBuffer, 0, StreamBuffer.Length)
 
-					StreamBuffer = Nothing
-					'_ms.SetLength(fsImage.Length)
-					'fsImage.Read(_ms.GetBuffer(), 0, Convert.ToInt32(fsImage.Length))
-					Me._ms.Flush()
-					_image = New Bitmap(Me._ms)
-				End Using
-			Catch ex As Exception
-				Master.eLog.WriteToErrorLog(ex.Message, ex.StackTrace, "Error: " & sPath)
-			End Try
-		End If
-	End Sub
+                    StreamBuffer = Nothing
+                    '_ms.SetLength(fsImage.Length)
+                    'fsImage.Read(_ms.GetBuffer(), 0, Convert.ToInt32(fsImage.Length))
+                    Me._ms.Flush()
+                    _image = New Bitmap(Me._ms)
+                End Using
+            Catch ex As Exception
+                Master.eLog.WriteToErrorLog(ex.Message, ex.StackTrace, "Error: " & sPath)
+            End Try
+        End If
+    End Sub
 
     Public Sub FromWeb(ByVal sURL As String)
         Try
@@ -1733,4 +1719,44 @@ Public Class Images
     End Function
 #End Region 'Methods
 
+#Region "IDisposable Support"
+    Private disposedValue As Boolean ' To detect redundant calls
+
+    ' IDisposable
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not Me.disposedValue Then
+            If disposing Then
+                ' dispose managed state (managed objects).
+                If _ms IsNot Nothing Then
+                    _ms.Flush()
+                    _ms.Close()
+                    _ms.Dispose()
+                End If
+                If _image IsNot Nothing Then _image.Dispose()
+                If sHTTP IsNot Nothing Then sHTTP.Dispose()
+            End If
+
+            ' free unmanaged resources (unmanaged objects) and override Finalize() below.
+            ' set large fields to null.
+            _ms = Nothing
+            _image = Nothing
+            sHTTP = Nothing
+        End If
+        Me.disposedValue = True
+    End Sub
+
+    ' TODO: override Finalize() only if Dispose(ByVal disposing As Boolean) above has code to free unmanaged resources.
+    'Protected Overrides Sub Finalize()
+    '    ' Do not change this code.  Put cleanup code in Dispose(ByVal disposing As Boolean) above.
+    '    Dispose(False)
+    '    MyBase.Finalize()
+    'End Sub
+
+    ' This code added by Visual Basic to correctly implement the disposable pattern.
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+        Dispose(True)
+        GC.SuppressFinalize(Me)
+    End Sub
+#End Region
 End Class
