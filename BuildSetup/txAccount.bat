8b803077784d062a48e22bde6f0de25911cd13e3
DanCooper <stephan.schueck@gmail.com>
2014-04-03 21:22:26 +0200

    added: InstallerScript
    This is only for the first test and not finished!

---
+++ BuildSetup/txAccount.bat
@@ -0,0 +1,16 @@
+ECHO OFF
+CLS
+ECHO ********************************************
+ECHO *   Language Downloader and Renamer for    *
+ECHO *           Ember Media Manager            *
+ECHO ********************************************
+ECHO *                                          *
+ECHO *  You need an existing Transifex account  *
+ECHO *                                          *
+ECHO *     Use the following as instance:       *
+ECHO *        https://www.transifex.com         *
+ECHO *                                          *
+ECHO ********************************************
+ECHO *               by DanCooper               *
+ECHO ********************************************
+tx init
