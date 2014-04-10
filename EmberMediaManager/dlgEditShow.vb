aa4f9218e7b604cf12d78ee639334169751d084c
DanCooper <stephan.schueck@gmail.com>
2014-04-10 22:06:22 +0200

    fixed: language issue (episode ordering)

--- EmberMediaManager/dlgEditShow.vb
+++ EmberMediaManager/dlgEditShow.vb
@@ -1449,7 +1449,7 @@ Public Class dlgEditShow
         Me.tpShowLandscape.Text = Master.eLang.GetString(1035, "Landscape")
         Me.tpShowPoster.Text = Master.eLang.GetString(148, "Poster")
 
-        Me.cbOrdering.Items.AddRange(New String() {Master.eLang.GetString(438, "Standard"), Master.eLang.GetString(350, "DVD"), Master.eLang.GetString(839, "Absolute")})
+        Me.cbOrdering.Items.AddRange(New String() {Master.eLang.GetString(438, "Standard"), Master.eLang.GetString(1067, "DVD"), Master.eLang.GetString(839, "Absolute")})
 
     End Sub
 
