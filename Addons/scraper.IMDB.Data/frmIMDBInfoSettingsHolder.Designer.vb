7e34d9e8b0ae7e08387d04099cce41c7eb4f6210
DanCooper <stephan.schueck@gmail.com>
2014-04-16 13:36:07 +0200

    fixed: IMDB trailer checkbox is disabled

--- Addons/scraper.IMDB.Data/frmIMDBInfoSettingsHolder.Designer.vb
+++ Addons/scraper.IMDB.Data/frmIMDBInfoSettingsHolder.Designer.vb
@@ -463,7 +463,6 @@ Partial Class frmIMDBInfoSettingsHolder
         'chkTrailer
         '
         Me.chkTrailer.AutoSize = True
-        Me.chkTrailer.Enabled = False
         Me.chkTrailer.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.chkTrailer.Location = New System.Drawing.Point(237, 36)
         Me.chkTrailer.Name = "chkTrailer"
