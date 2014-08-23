1bd67036cca2756620ba54fa7deace75ca677761
DanCooper <stephan.schueck@gmail.com>
2014-08-23 15:45:42 +0200

    code: cleanup

--- EmberMediaManager/dlgEditMovie.vb
+++ EmberMediaManager/dlgEditMovie.vb
@@ -1665,19 +1665,11 @@ Public Class dlgEditMovie
                 End If
 
                 If Not String.IsNullOrEmpty(Master.currMovie.Movie.OriginalTitle) Then
-                    'cocotus, 2014/08/16 Bug report #96  -  Original Title behaviour - Fix: Display the current saved(nfo/database) originaltitle/sortitle in Edit Movie Page (without conditions(title behaviour))
                     .txtOriginalTitle.Text = Master.currMovie.Movie.OriginalTitle
-                    ' If Master.currMovie.Movie.OriginalTitle <> StringUtils.FilterTokens_Movie(Master.currMovie.Movie.Title) Then
-                    '  .txtOriginalTitle.Text = Master.currMovie.Movie.OriginalTitle
-                    '   End If
                 End If
 
                 If Not String.IsNullOrEmpty(Master.currMovie.Movie.SortTitle) Then
-                    'cocotus, 2014/08/16 Bug report #96  -  Original Title behaviour - Fix: Display the current saved(nfo/database) originaltitle/sortitle in Edit Movie Page (without conditions(title behaviour))
                     .txtSortTitle.Text = Master.currMovie.Movie.SortTitle
-                    'If Master.currMovie.Movie.SortTitle <> StringUtils.FilterTokens_Movie(Master.currMovie.Movie.Title) Then
-                    '    .txtSortTitle.Text = Master.currMovie.Movie.SortTitle
-                    'End If
                 End If
 
                 If Not String.IsNullOrEmpty(Master.currMovie.Movie.Tagline) Then
@@ -2883,21 +2875,16 @@ Public Class dlgEditMovie
                 Master.currMovie.IsMark = Me.chkMark.Checked
 
                 If Not String.IsNullOrEmpty(.txtTitle.Text) Then
+                    Master.currMovie.Movie.Title = .txtTitle.Text.Trim
+
                     If Master.eSettings.MovieDisplayYear AndAlso Not String.IsNullOrEmpty(.mtxtYear.Text.Trim) Then
                         Master.currMovie.ListTitle = String.Format("{0} ({1})", StringUtils.FilterTokens_Movie(.txtTitle.Text.Trim), .mtxtYear.Text.Trim)
                     Else
                         Master.currMovie.ListTitle = StringUtils.FilterTokens_Movie(.txtTitle.Text.Trim)
                     End If
-                    Master.currMovie.Movie.Title = .txtTitle.Text.Trim
                 End If
 
-                If Not String.IsNullOrEmpty(.txtOriginalTitle.Text) Then
-                    Master.currMovie.Movie.OriginalTitle = .txtOriginalTitle.Text.Trim
-                Else
-                    'cocotus, 2014/08/16 Bug report #96  -  Original Title behaviour - Fix: Don't use sorted title for originaltitle if no originaltitle is set - leave originaltitle empty 
-                    '  Master.currMovie.Movie.OriginalTitle = StringUtils.FilterTokens_Movie(.txtTitle.Text.Trim)
-                    Master.currMovie.Movie.OriginalTitle = ""
-                End If
+                Master.currMovie.Movie.OriginalTitle = .txtOriginalTitle.Text.Trim
 
                 If Not String.IsNullOrEmpty(.txtSortTitle.Text) Then
                     Master.currMovie.Movie.SortTitle = .txtSortTitle.Text.Trim
