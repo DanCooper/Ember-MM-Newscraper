9f320ac526bc7f16f60cae099ed0a2209d24d15e
DanCooper <stephan.schueck@gmail.com>
2014-10-21 12:03:55 +0200

    release: 1.4.0.5

--- EmberAPI/My Project/AssemblyInfo.vb
+++ EmberAPI/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.4.0.3")> 
-<Assembly: AssemblyFileVersion("1.4.0.3")> 
+<Assembly: AssemblyVersion("1.4.0.5")> 
+<Assembly: AssemblyFileVersion("1.4.0.5")> 
--- EmberMediaManager/My Project/AssemblyInfo.vb
+++ EmberMediaManager/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.4.0.4")> 
-<Assembly: AssemblyFileVersion("1.4.0.4")> 
+<Assembly: AssemblyVersion("1.4.0.5")> 
+<Assembly: AssemblyFileVersion("1.4.0.5")> 
