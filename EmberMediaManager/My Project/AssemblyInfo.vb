73d57e2da19a8d58415364bce1256339002c0525
DanCooper <stephan.schueck@gmail.com>
2014-10-26 17:36:25 +0100

    release: 1.4.0.6

--- EmberMediaManager/My Project/AssemblyInfo.vb
+++ EmberMediaManager/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.4.0.5")> 
-<Assembly: AssemblyFileVersion("1.4.0.5")> 
+<Assembly: AssemblyVersion("1.4.0.6")> 
+<Assembly: AssemblyFileVersion("1.4.0.6")> 
