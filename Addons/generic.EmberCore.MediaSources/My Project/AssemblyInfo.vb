c426550ce96ae533233b0a992208b826aa850d72
DanCooper <stephan.schueck@gmail.com>
2014-11-27 11:13:29 +0100

    release: 1.4.0.7

--- Addons/generic.EmberCore.MediaSources/My Project/AssemblyInfo.vb
+++ Addons/generic.EmberCore.MediaSources/My Project/AssemblyInfo.vb
@@ -27,5 +27,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
-<Assembly: AssemblyVersion("1.4.0.1")> 
-<Assembly: AssemblyFileVersion("1.4.0.1")> 
+<Assembly: AssemblyVersion("1.4.0.7")> 
+<Assembly: AssemblyFileVersion("1.4.0.7")> 
