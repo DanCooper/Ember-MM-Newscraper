e4eb553a90d40ce540c53b80e776c2bab66ed595
Cocotus <biberas@gmail.com>
2015-01-29 00:57:18 +0100

    Fixed/Added: trakt.tv Userlistexplorer

---
+++ Trakttv/Model/Other/TraktNetworkUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Trakttv.TraktAPI.Model
+{
+    [DataContract]
+    public class TraktNetworkUser
+    {
+        [DataMember(Name = "followed_at")]
+        public string FollowedAt { get; set; }
+
+        [DataMember(Name = "user")]
+        public TraktUserSummary User { get; set; }
+    }
+}
