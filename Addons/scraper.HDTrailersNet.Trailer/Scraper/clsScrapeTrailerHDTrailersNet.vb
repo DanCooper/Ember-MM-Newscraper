2f5bb511e23d2124fbdec2b402da68a4c0a4be07
Cocotus <biberas@gmail.com>
2015-02-09 21:32:55 +0100

    Fixed: HDTrailersNet_Trailer scraper, improved matchrate

--- Addons/scraper.HDTrailersNet.Trailer/Scraper/clsScrapeTrailerHDTrailersNet.vb
+++ Addons/scraper.HDTrailersNet.Trailer/Scraper/clsScrapeTrailerHDTrailersNet.vb
@@ -30,15 +30,17 @@ Namespace HDTrailersNet
         Shared logger As Logger = NLog.LogManager.GetCurrentClassLogger()
 
         Private originaltitle As String
+        Private title As String
         Private _trailerlist As New List(Of Trailers)
 
 #End Region 'Fields
 
 #Region "Constructors"
 
-        Public Sub New(ByVal sOriginalTitle As String)
+        Public Sub New(ByVal sOriginalTitle As String, ByVal stitle As String)
             Clear()
             originaltitle = sOriginalTitle
+            title = stitle
             GetMovieTrailers()
         End Sub
 
@@ -80,6 +82,9 @@ Namespace HDTrailersNet
         ''''   <td class="bottomTableResolution"><a href="http://trailers.apple.com/movies/wb/terminatorsalvation/terminatorsalvation-tlr1_h1080p.mov" rel="lightbox[res1080p 1920 1080]" title="Terminator Salvation - Trailer 1 - 1080p">1080p</a></td>
         ''''   --> we can extract URL and title
         ''' 4. Add found trailerlink to global list
+        '  ''' <td class="bottomTableResolution"><a href="http://videos.hd-trailers.net/scarface-bluray-trailer-480p-HDTN.mov" rel="lightbox[res480p 852 480]" title="Scarface - Blu-ray Trailer - 480p">480p</a></td>
+        '<td class="bottomTableResolution"><a href="http://videos.hd-trailers.net/scarface-bluray-trailer-720p-HDTN.mov" rel="lightbox[res720p 1280 720]" title="Scarface - Blu-ray Trailer - 720p">720p</a></td>
+        '<td class="bottomTableResolution"><a href="http://videos.hd-trailers.net/scarface-bluray-trailer-1080p-HDTN.mov" rel="lightbox[res1080p 1920 1080]" title="Scarface - Blu-ray Trailer - 1080p">1080p</a></td>
         ''' </remarks>
         Private Sub GetMovieTrailers()
             Try
@@ -96,56 +101,73 @@ Namespace HDTrailersNet
                     sHTTP = Nothing
                     'Step 2: If trailerlink was found -> ok, else use Google to search for trailer on hd-trailer.net
                     If sHtml <> "" Then
-                        TrailerWEBPageURL = "http://www.hd-trailers.net/movie/" & searchtitle & "/"
+                        TrailerWEBPageURL = "http://www.hd-trailers.net/movie/" & StringUtils.RemovePunctuation(title).Replace("+", "-").Replace(" ", "-") & "/"
                         logger.Debug("[" & originaltitle & "] HD-Trailer.net - Movie found on http://www.hd-trailers.net - URL: " & TrailerWEBPageURL)
                     Else
-                        logger.Debug("[" & originaltitle & "] HD-Trailer.net - Movie NOT found on http://www.hd-trailers.net - URL: " & "http://www.hd-trailers.net/movie/" & searchtitle & "/")
-                        'Google search
-                        'Warning: will be blocked by Google when spamming webrequests!
-                        Dim BaseURL As String = "http://www.google.ch/search?q="
-                        Dim searchTerm As String = "site:http://www.hd-trailers.net/ " + """" + originaltitle + """"
-                        searchTerm = Web.HttpUtility.UrlEncode(searchTerm)
-                        SearchURL = String.Concat(BaseURL, searchTerm)
-                        '--> i.e "
-                        'performing google search to find avalaible links on hd-trailers.net
+                        'try with title instead of originaltitle!
                         sHTTP = New HTTP
-                        sHtml = sHTTP.DownloadData(SearchURL)
+                        sHtml = sHTTP.DownloadData("http://www.hd-trailers.net/movie/" & StringUtils.RemovePunctuation(title).Replace("+", "-").Replace(" ", "-") & "/")
                         sHTTP = Nothing
                         If sHtml <> "" Then
-                            'Now extract links from googleresults
-                            Dim trailerWEBURLResults As MatchCollection = Nothing
-                            Dim googleresultPattern As String = "<a href=""/url\?q=(?<RESULTS>.*?)\/&amp;" 'Google search results
-                            trailerWEBURLResults = Regex.Matches(sHtml, googleresultPattern, RegexOptions.Singleline)
-                            'Go through each searchresults from google and compare found URL with originaltitle using Levenshtein algorithm
-                            'check if something was found
-                            If Not trailerWEBURLResults Is Nothing AndAlso trailerWEBURLResults.Count > 0 Then
-                                Dim compareresult As Integer = 100
-                                Dim comparestring As String = ""
-                                For Each searchresult As Match In trailerWEBURLResults
-                                    TrailerWEBPageURL = ""
-                                    TrailerWEBPageURL = searchresult.Groups(1).Value
-                                    If Not String.IsNullOrEmpty(TrailerWEBPageURL) Then
-                                        If TrailerWEBPageURL.Contains("/") Then
-                                            comparestring = TrailerWEBPageURL.Remove(0, TrailerWEBPageURL.LastIndexOf("/") + 1)
-                                            compareresult = 100
-                                            searchtitle = (StringUtils.RemovePunctuation(originaltitle.ToLower)).Replace(" ", "")
-                                            comparestring = (StringUtils.RemovePunctuation(comparestring.ToLower)).Replace(" ", "")
-                                            compareresult = StringUtils.ComputeLevenshtein(comparestring, searchtitle)
-                                            If (originaltitle.Length <= 5 AndAlso compareresult = 0) OrElse (originaltitle.Length > 5 AndAlso compareresult <= 2) Then
-                                                logger.Debug("[" & originaltitle & "] HD-Trailer.net - Google: Movie found! URL: " & TrailerWEBPageURL)
-                                                Exit For
-                                            Else
-                                                logger.Debug("[" & originaltitle & "] HD-Trailer.net - Google: wrong result! URL: " & TrailerWEBPageURL)
-                                                TrailerWEBPageURL = ""
+                            TrailerWEBPageURL = "http://www.hd-trailers.net/movie/" & StringUtils.RemovePunctuation(title).Replace("+", "-").Replace(" ", "-") & "/"
+                            logger.Debug("[" & originaltitle & "] HD-Trailer.net - Movie found on http://www.hd-trailers.net - URL: " & TrailerWEBPageURL)
+                        Else
+                            logger.Debug("[" & originaltitle & "] HD-Trailer.net - Movie NOT found on http://www.hd-trailers.net - URL: " & "http://www.hd-trailers.net/movie/" & searchtitle & "/")
+                            'Google search
+                            'Warning: will be blocked by Google when spamming webrequests!
+                            Dim BaseURL As String = "http://www.google.ch/search?q="
+                            Dim searchTerm As String = "site:http://www.hd-trailers.net/ " + """" + originaltitle + """"
+                            searchTerm = Web.HttpUtility.UrlEncode(searchTerm)
+                            SearchURL = String.Concat(BaseURL, searchTerm)
+                            '--> i.e "
+                            'performing google search to find avalaible links on hd-trailers.net
+                            sHTTP = New HTTP
+                            sHtml = sHTTP.DownloadData(SearchURL)
+                            sHTTP = Nothing
+                            If sHtml <> "" Then
+                                'Now extract links from googleresults
+                                Dim trailerWEBURLResults As MatchCollection = Nothing
+                                Dim googleresultPattern As String = "<a href=""/url\?q=(?<RESULTS>.*?)\/&amp;" 'Google search results
+                                trailerWEBURLResults = Regex.Matches(sHtml, googleresultPattern, RegexOptions.Singleline)
+                                'Go through each searchresults from google and compare found URL with originaltitle using Levenshtein algorithm
+                                'check if something was found
+                                If Not trailerWEBURLResults Is Nothing AndAlso trailerWEBURLResults.Count > 0 Then
+                                    Dim compareresult As Integer = 100
+                                    Dim comparestring As String = ""
+                                    For Each searchresult As Match In trailerWEBURLResults
+                                        TrailerWEBPageURL = ""
+                                        TrailerWEBPageURL = searchresult.Groups(1).Value
+                                        If Not String.IsNullOrEmpty(TrailerWEBPageURL) Then
+                                            If TrailerWEBPageURL.Contains("/") Then
+                                                comparestring = TrailerWEBPageURL.Remove(0, TrailerWEBPageURL.LastIndexOf("/") + 1)
+                                                compareresult = 100
+                                                searchtitle = (StringUtils.RemovePunctuation(originaltitle.ToLower)).Replace(" ", "")
+                                                comparestring = (StringUtils.RemovePunctuation(comparestring.ToLower)).Replace(" ", "")
+                                                compareresult = StringUtils.ComputeLevenshtein(comparestring, searchtitle)
+                                                If (originaltitle.Length <= 5 AndAlso compareresult = 0) OrElse (originaltitle.Length > 5 AndAlso compareresult <= 2) Then
+                                                    logger.Debug("[" & originaltitle & "] HD-Trailer.net - Google: Movie found! URL: " & TrailerWEBPageURL)
+                                                    Exit For
+                                                Else
+                                                    logger.Debug("[" & originaltitle & "] HD-Trailer.net - Google: wrong result! URL: " & TrailerWEBPageURL)
+                                                    TrailerWEBPageURL = ""
+                                                End If
+                                                compareresult = StringUtils.ComputeLevenshtein(comparestring, (StringUtils.RemovePunctuation(title.ToLower)).Replace(" ", ""))
+                                                If (originaltitle.Length <= 5 AndAlso compareresult = 0) OrElse (originaltitle.Length > 5 AndAlso compareresult <= 2) Then
+                                                    logger.Debug("[" & originaltitle & "] HD-Trailer.net - Google: Movie found! URL: " & TrailerWEBPageURL)
+                                                    Exit For
+                                                Else
+                                                    logger.Debug("[" & originaltitle & "] HD-Trailer.net - Google: wrong result! URL: " & TrailerWEBPageURL)
+                                                    TrailerWEBPageURL = ""
+                                                End If
                                             End If
                                         End If
-                                    End If
-                                Next
+                                    Next
+                                Else
+                                    logger.Debug("[" & originaltitle & "] HD-Trailer.net - Movie NOT found on http://www.hd-trailers.net using google! URL: " & SearchURL)
+                                End If
                             Else
                                 logger.Debug("[" & originaltitle & "] HD-Trailer.net - Movie NOT found on http://www.hd-trailers.net using google! URL: " & SearchURL)
                             End If
-                        Else
-                            logger.Debug("[" & originaltitle & "] HD-Trailer.net - Movie NOT found on http://www.hd-trailers.net using google! URL: " & SearchURL)
                         End If
                     End If
 
@@ -166,7 +188,7 @@ Namespace HDTrailersNet
                             For Each item As Match In trailerURLResults
                                 If item.Groups.Count > 0 Then
                                     'only accept mov and mp4 files for now
-                                    If Not item.Groups(1).Value Is Nothing AndAlso (item.Groups(1).Value.EndsWith("p.mov") OrElse item.Groups(1).Value.EndsWith("p.mp4")) Then
+                                    If Not item.Groups(1).Value Is Nothing AndAlso (item.Groups(1).Value.EndsWith(".mov") OrElse item.Groups(1).Value.EndsWith(".mp4")) Then
                                         Dim trailer As New Trailers
                                         'trailer description
                                         If Not item.Groups(2).Value Is Nothing Then
