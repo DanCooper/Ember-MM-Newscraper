2c49df340719a7598e70f9592630597ed29e2cfa
DanCooper <stephan.schueck@gmail.com>
2015-03-05 22:39:36 +0100

    fixed: Change Ep dialog size

--- Addons/scraper.TVDB.Poster/TVScraper/dlgTVChangeEp.Designer.vb
+++ Addons/scraper.TVDB.Poster/TVScraper/dlgTVChangeEp.Designer.vb
@@ -30,12 +30,20 @@
         Me.pbPreview = New System.Windows.Forms.PictureBox()
         Me.lblTitle = New System.Windows.Forms.Label()
         Me.txtPlot = New System.Windows.Forms.TextBox()
+        Me.pnlBottom = New System.Windows.Forms.Panel()
+        Me.tblBottom = New System.Windows.Forms.TableLayoutPanel()
+        Me.pnlMain = New System.Windows.Forms.Panel()
+        Me.tblMain = New System.Windows.Forms.TableLayoutPanel()
         CType(Me.pbPreview, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.pnlBottom.SuspendLayout()
+        Me.tblBottom.SuspendLayout()
+        Me.pnlMain.SuspendLayout()
+        Me.tblMain.SuspendLayout()
         Me.SuspendLayout()
         '
         'OK_Button
         '
-        Me.OK_Button.Location = New System.Drawing.Point(505, 286)
+        Me.OK_Button.Location = New System.Drawing.Point(497, 3)
         Me.OK_Button.Name = "OK_Button"
         Me.OK_Button.Size = New System.Drawing.Size(67, 23)
         Me.OK_Button.TabIndex = 0
@@ -44,7 +52,7 @@
         'Cancel_Button
         '
         Me.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.Cancel_Button.Location = New System.Drawing.Point(575, 286)
+        Me.Cancel_Button.Location = New System.Drawing.Point(570, 3)
         Me.Cancel_Button.Name = "Cancel_Button"
         Me.Cancel_Button.Size = New System.Drawing.Size(67, 23)
         Me.Cancel_Button.TabIndex = 1
@@ -53,12 +61,14 @@
         'lvEpisodes
         '
         Me.lvEpisodes.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.colEpisode, Me.colTitle})
+        Me.lvEpisodes.Dock = System.Windows.Forms.DockStyle.Fill
         Me.lvEpisodes.FullRowSelect = True
         Me.lvEpisodes.HideSelection = False
         Me.lvEpisodes.Location = New System.Drawing.Point(3, 3)
         Me.lvEpisodes.MultiSelect = False
         Me.lvEpisodes.Name = "lvEpisodes"
-        Me.lvEpisodes.Size = New System.Drawing.Size(358, 278)
+        Me.tblMain.SetRowSpan(Me.lvEpisodes, 3)
+        Me.lvEpisodes.Size = New System.Drawing.Size(358, 399)
         Me.lvEpisodes.TabIndex = 2
         Me.lvEpisodes.UseCompatibleStateImageBehavior = False
         Me.lvEpisodes.View = System.Windows.Forms.View.Details
@@ -74,7 +84,8 @@
         '
         'pbPreview
         '
-        Me.pbPreview.Location = New System.Drawing.Point(417, 12)
+        Me.pbPreview.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.pbPreview.Location = New System.Drawing.Point(405, 3)
         Me.pbPreview.Name = "pbPreview"
         Me.pbPreview.Size = New System.Drawing.Size(174, 117)
         Me.pbPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
@@ -83,22 +94,85 @@
         '
         'lblTitle
         '
+        Me.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.lblTitle.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblTitle.Location = New System.Drawing.Point(367, 140)
+        Me.lblTitle.Location = New System.Drawing.Point(372, 123)
         Me.lblTitle.Name = "lblTitle"
-        Me.lblTitle.Size = New System.Drawing.Size(275, 21)
+        Me.lblTitle.Padding = New System.Windows.Forms.Padding(0, 5, 0, 5)
+        Me.lblTitle.Size = New System.Drawing.Size(240, 26)
         Me.lblTitle.TabIndex = 3
+        Me.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
         'txtPlot
         '
         Me.txtPlot.BackColor = System.Drawing.SystemColors.Control
         Me.txtPlot.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.txtPlot.Location = New System.Drawing.Point(366, 169)
+        Me.txtPlot.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtPlot.Location = New System.Drawing.Point(367, 152)
         Me.txtPlot.Multiline = True
         Me.txtPlot.Name = "txtPlot"
         Me.txtPlot.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.txtPlot.Size = New System.Drawing.Size(276, 111)
+        Me.txtPlot.Size = New System.Drawing.Size(250, 250)
         Me.txtPlot.TabIndex = 4
+        '
+        'pnlBottom
+        '
+        Me.pnlBottom.AutoSize = True
+        Me.pnlBottom.Controls.Add(Me.tblBottom)
+        Me.pnlBottom.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.pnlBottom.Location = New System.Drawing.Point(0, 425)
+        Me.pnlBottom.Name = "pnlBottom"
+        Me.pnlBottom.Size = New System.Drawing.Size(640, 29)
+        Me.pnlBottom.TabIndex = 5
+        '
+        'tblBottom
+        '
+        Me.tblBottom.AutoSize = True
+        Me.tblBottom.ColumnCount = 3
+        Me.tblBottom.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.tblBottom.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.tblBottom.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.tblBottom.Controls.Add(Me.Cancel_Button, 2, 0)
+        Me.tblBottom.Controls.Add(Me.OK_Button, 1, 0)
+        Me.tblBottom.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tblBottom.Location = New System.Drawing.Point(0, 0)
+        Me.tblBottom.Name = "tblBottom"
+        Me.tblBottom.RowCount = 1
+        Me.tblBottom.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.tblBottom.Size = New System.Drawing.Size(640, 29)
+        Me.tblBottom.TabIndex = 0
+        '
+        'pnlMain
+        '
+        Me.pnlMain.Controls.Add(Me.tblMain)
+        Me.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlMain.Location = New System.Drawing.Point(0, 0)
+        Me.pnlMain.Name = "pnlMain"
+        Me.pnlMain.Size = New System.Drawing.Size(640, 425)
+        Me.pnlMain.TabIndex = 6
+        '
+        'tblMain
+        '
+        Me.tblMain.AutoScroll = True
+        Me.tblMain.AutoSize = True
+        Me.tblMain.ColumnCount = 3
+        Me.tblMain.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.tblMain.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.tblMain.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.tblMain.Controls.Add(Me.lblTitle, 1, 1)
+        Me.tblMain.Controls.Add(Me.pbPreview, 1, 0)
+        Me.tblMain.Controls.Add(Me.txtPlot, 1, 2)
+        Me.tblMain.Controls.Add(Me.lvEpisodes, 0, 0)
+        Me.tblMain.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tblMain.Location = New System.Drawing.Point(0, 0)
+        Me.tblMain.Name = "tblMain"
+        Me.tblMain.RowCount = 4
+        Me.tblMain.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.tblMain.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.tblMain.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.tblMain.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.tblMain.Size = New System.Drawing.Size(640, 425)
+        Me.tblMain.TabIndex = 0
         '
         'dlgTVChangeEp
         '
@@ -106,22 +180,24 @@
         Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
         Me.CancelButton = Me.Cancel_Button
-        Me.ClientSize = New System.Drawing.Size(646, 315)
+        Me.ClientSize = New System.Drawing.Size(640, 454)
         Me.ControlBox = False
-        Me.Controls.Add(Me.txtPlot)
-        Me.Controls.Add(Me.lblTitle)
-        Me.Controls.Add(Me.pbPreview)
-        Me.Controls.Add(Me.lvEpisodes)
-        Me.Controls.Add(Me.OK_Button)
-        Me.Controls.Add(Me.Cancel_Button)
+        Me.Controls.Add(Me.pnlMain)
+        Me.Controls.Add(Me.pnlBottom)
         Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "dlgTVChangeEp"
         Me.ShowInTaskbar = False
         Me.Text = "Change Episode"
         CType(Me.pbPreview, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.pnlBottom.ResumeLayout(False)
+        Me.pnlBottom.PerformLayout()
+        Me.tblBottom.ResumeLayout(False)
+        Me.pnlMain.ResumeLayout(False)
+        Me.pnlMain.PerformLayout()
+        Me.tblMain.ResumeLayout(False)
+        Me.tblMain.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -134,5 +210,9 @@
     Friend WithEvents pbPreview As System.Windows.Forms.PictureBox
     Friend WithEvents lblTitle As System.Windows.Forms.Label
     Friend WithEvents txtPlot As System.Windows.Forms.TextBox
+    Friend WithEvents pnlBottom As System.Windows.Forms.Panel
+    Friend WithEvents tblBottom As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents tblMain As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents pnlMain As System.Windows.Forms.Panel
 
 End Class
