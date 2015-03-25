144adf0c285b11c9a6baefb499084588738dfa88
DanCooper <stephan.schueck@gmail.com>
2015-03-25 18:57:32 +0100

    release: 1.4.3.0
    disabled FilterEditor (does not work yet)

--- Addons/scraper.HDTrailersNet.Trailer/My Project/AssemblyInfo.vb
+++ Addons/scraper.HDTrailersNet.Trailer/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.4.0.7")> 
-<Assembly: AssemblyFileVersion("1.4.0.7")> 
+<Assembly: AssemblyVersion("1.4.3.0")> 
+<Assembly: AssemblyFileVersion("1.4.3.0")> 
