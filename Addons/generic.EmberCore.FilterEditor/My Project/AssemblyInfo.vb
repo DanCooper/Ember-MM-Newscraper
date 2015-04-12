72c8657dd4879312947501678096efd265a6afd8
DanCooper <stephan.schueck@gmail.com>
2015-04-12 22:55:36 +0200

    release: 1.4.4.0

--- Addons/generic.EmberCore.FilterEditor/My Project/AssemblyInfo.vb
+++ Addons/generic.EmberCore.FilterEditor/My Project/AssemblyInfo.vb
@@ -27,5 +27,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
-<Assembly: AssemblyVersion("1.4.2.0")> 
-<Assembly: AssemblyFileVersion("1.4.2.0")> 
+<Assembly: AssemblyVersion("1.4.4.0")> 
+<Assembly: AssemblyFileVersion("1.4.4.0")> 
