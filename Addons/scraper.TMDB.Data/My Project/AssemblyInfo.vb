a6d3e72c673ba4f49cbf57da66c952b3b2a12fcc
Dan Cooper <stephan.schueck@gmail.com>
2015-05-08 00:01:12 +0200

    release: 1.4.5.0

--- Addons/scraper.TMDB.Data/My Project/AssemblyInfo.vb
+++ Addons/scraper.TMDB.Data/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.4.3.1")> 
-<Assembly: AssemblyFileVersion("1.4.3.1")> 
+<Assembly: AssemblyVersion("1.4.5.1")> 
+<Assembly: AssemblyFileVersion("1.4.5.1")> 
