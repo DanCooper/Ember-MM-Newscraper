22f7544bab3bad5cde95c89b5c532dc5360ce663
DanCooper <stephan.schueck@gmail.com>
2015-08-19 12:43:10 +0200

    added: YouTube trailer scraper

---
+++ Addons/scraper.Trailer.YouTube/YouTube_trailer.vb
@@ -0,0 +1,175 @@
+﻿' ################################################################################
+' #                             EMBER MEDIA MANAGER                              #
+' ################################################################################
+' ################################################################################
+' # This file is part of Ember Media Manager.                                    #
+' #                                                                              #
+' # Ember Media Manager is free software: you can redistribute it and/or modify  #
+' # it under the terms of the GNU General Public License as published by         #
+' # the Free Software Foundation, either version 3 of the License, or            #
+' # (at your option) any later version.                                          #
+' #                                                                              #
+' # Ember Media Manager is distributed in the hope that it will be useful,       #
+' # but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+' # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+' # GNU General Public License for more details.                                 #
+' #                                                                              #
+' # You should have received a copy of the GNU General Public License            #
+' # along with Ember Media Manager.  If not, see <http://www.gnu.org/licenses/>. #
+' ###############################################################################
+
+Imports EmberAPI
+Imports NLog
+
+Public Class YouTube_trailer
+    Implements Interfaces.ScraperModule_Trailer_Movie
+
+
+#Region "Fields"
+    Shared logger As Logger = NLog.LogManager.GetCurrentClassLogger()
+
+    Public Shared ConfigScrapeModifier As New Structures.ScrapeModifier
+    Public Shared _AssemblyName As String
+
+    Private _SpecialSettings As New SpecialSettings
+    Private _Name As String = "YouTube_Trailer"
+    Private _ScraperEnabled As Boolean = False
+    Private _setup As frmSettingsHolder
+
+#End Region 'Fields
+
+#Region "Events"
+
+    Public Event ModuleSettingsChanged() Implements Interfaces.ScraperModule_Trailer_Movie.ModuleSettingsChanged
+    Public Event MovieScraperEvent(ByVal eType As Enums.ScraperEventType, ByVal Parameter As Object) Implements Interfaces.ScraperModule_Trailer_Movie.ScraperEvent
+    Public Event SetupScraperChanged(ByVal name As String, ByVal State As Boolean, ByVal difforder As Integer) Implements Interfaces.ScraperModule_Trailer_Movie.ScraperSetupChanged
+    Public Event SetupNeedsRestart() Implements Interfaces.ScraperModule_Trailer_Movie.SetupNeedsRestart
+
+#End Region 'Events
+
+#Region "Properties"
+
+    ReadOnly Property ModuleName() As String Implements Interfaces.ScraperModule_Trailer_Movie.ModuleName
+        Get
+            Return _Name
+        End Get
+    End Property
+
+    ReadOnly Property ModuleVersion() As String Implements Interfaces.ScraperModule_Trailer_Movie.ModuleVersion
+        Get
+            Return System.Diagnostics.FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly.Location).FileVersion.ToString
+        End Get
+    End Property
+
+    Property ScraperEnabled() As Boolean Implements Interfaces.ScraperModule_Trailer_Movie.ScraperEnabled
+        Get
+            Return _ScraperEnabled
+        End Get
+        Set(ByVal value As Boolean)
+            _ScraperEnabled = value
+        End Set
+    End Property
+
+#End Region 'Properties
+
+#Region "Methods"
+    Private Sub Handle_ModuleSettingsChanged()
+        RaiseEvent ModuleSettingsChanged()
+    End Sub
+
+    Private Sub Handle_SetupNeedsRestart()
+        RaiseEvent SetupNeedsRestart()
+    End Sub
+
+    Private Sub Handle_SetupScraperChanged(ByVal state As Boolean, ByVal difforder As Integer)
+        ScraperEnabled = state
+        RaiseEvent SetupScraperChanged(String.Concat(Me._Name, "Scraper"), state, difforder)
+    End Sub
+
+    Sub Init(ByVal sAssemblyName As String) Implements Interfaces.ScraperModule_Trailer_Movie.Init
+        _AssemblyName = sAssemblyName
+        LoadSettings()
+    End Sub
+
+    Function InjectSetupScraper() As Containers.SettingsPanel Implements Interfaces.ScraperModule_Trailer_Movie.InjectSetupScraper
+        Dim Spanel As New Containers.SettingsPanel
+        _setup = New frmSettingsHolder
+        LoadSettings()
+        _setup.chkEnabled.Checked = _ScraperEnabled
+
+        _setup.orderChanged()
+
+        Spanel.Name = String.Concat(Me._Name, "Scraper")
+        Spanel.Text = "YouTube"
+        Spanel.Prefix = "YouTubeTrailer_"
+        Spanel.Order = 110
+        Spanel.Parent = "pnlMovieTrailer"
+        Spanel.Type = Master.eLang.GetString(36, "Movies")
+        Spanel.ImageIndex = If(Me._ScraperEnabled, 9, 10)
+        Spanel.Panel = Me._setup.pnlSettings
+
+        AddHandler _setup.SetupScraperChanged, AddressOf Handle_SetupScraperChanged
+        AddHandler _setup.ModuleSettingsChanged, AddressOf Handle_ModuleSettingsChanged
+        AddHandler _setup.SetupNeedsRestart, AddressOf Handle_SetupNeedsRestart
+        Return Spanel
+    End Function
+
+    Sub LoadSettings()
+
+        ConfigScrapeModifier.MainTrailer = clsAdvancedSettings.GetBooleanSetting("DoTrailer", True)
+
+    End Sub
+
+    Function Scraper(ByRef DBMovie As Database.DBElement, ByVal Type As Enums.ModifierType, ByRef TrailerList As List(Of MediaContainers.Trailer)) As Interfaces.ModuleResult Implements Interfaces.ScraperModule_Trailer_Movie.Scraper
+        logger.Trace("Started scrape", New StackTrace().ToString())
+
+        LoadSettings()
+
+        If Not String.IsNullOrEmpty(DBMovie.Movie.Title) Then
+            Dim _scraper As New YouTubes.Scraper()
+
+            TrailerList = _scraper.GetTrailers(DBMovie.Movie.Title)
+        End If
+
+        logger.Trace("Finished scrape", New StackTrace().ToString())
+        Return New Interfaces.ModuleResult With {.breakChain = False}
+    End Function
+
+    Sub SaveSettings()
+        Using settings = New clsAdvancedSettings()
+            settings.SetBooleanSetting("DoTrailer", ConfigScrapeModifier.MainTrailer)
+        End Using
+    End Sub
+
+    Sub SaveSetupScraper(ByVal DoDispose As Boolean) Implements Interfaces.ScraperModule_Trailer_Movie.SaveSetupScraper
+        SaveSettings()
+        'ModulesManager.Instance.SaveSettings()
+        If DoDispose Then
+            RemoveHandler _setup.SetupScraperChanged, AddressOf Handle_SetupScraperChanged
+            RemoveHandler _setup.ModuleSettingsChanged, AddressOf Handle_ModuleSettingsChanged
+            RemoveHandler _setup.SetupNeedsRestart, AddressOf Handle_SetupNeedsRestart
+            _setup.Dispose()
+        End If
+    End Sub
+
+    Public Sub ScraperOrderChanged() Implements EmberAPI.Interfaces.ScraperModule_Trailer_Movie.ScraperOrderChanged
+        _setup.orderChanged()
+    End Sub
+
+#End Region 'Methods
+
+#Region "Nested Types"
+
+    Structure SpecialSettings
+
+#Region "Fields"
+
+        Dim PrefLanguage As String
+
+#End Region 'Fields
+
+    End Structure
+
+#End Region 'Nested Types
+
+End Class
