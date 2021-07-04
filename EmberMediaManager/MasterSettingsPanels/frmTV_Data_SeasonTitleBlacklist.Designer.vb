f601c3f9604042757bbb2f6bc4ab2f6e7b8bae32
DanCooper <stephan.schueck@gmail.com>
2021-07-04 18:17:07 +0200

    improved: season title blacklist dialog size

--- EmberMediaManager/MasterSettingsPanels/frmTV_Data_SeasonTitleBlacklist.Designer.vb
+++ EmberMediaManager/MasterSettingsPanels/frmTV_Data_SeasonTitleBlacklist.Designer.vb
@@ -52,7 +52,7 @@ Partial Class frmTV_Data_SeasonTitleBlacklist
         Me.pnlTop.Dock = System.Windows.Forms.DockStyle.Top
         Me.pnlTop.Location = New System.Drawing.Point(0, 0)
         Me.pnlTop.Name = "pnlTop"
-        Me.pnlTop.Size = New System.Drawing.Size(384, 26)
+        Me.pnlTop.Size = New System.Drawing.Size(484, 36)
         Me.pnlTop.TabIndex = 3
         '
         'tblTop
@@ -67,7 +67,7 @@ Partial Class frmTV_Data_SeasonTitleBlacklist
         Me.tblTop.RowCount = 1
         Me.tblTop.RowStyles.Add(New System.Windows.Forms.RowStyle())
         Me.tblTop.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26.0!))
-        Me.tblTop.Size = New System.Drawing.Size(384, 26)
+        Me.tblTop.Size = New System.Drawing.Size(484, 36)
         Me.tblTop.TabIndex = 0
         '
         'lblHint
@@ -75,18 +75,20 @@ Partial Class frmTV_Data_SeasonTitleBlacklist
         Me.lblHint.AutoSize = True
         Me.lblHint.Location = New System.Drawing.Point(3, 0)
         Me.lblHint.Name = "lblHint"
-        Me.lblHint.Size = New System.Drawing.Size(425, 26)
+        Me.lblHint.Padding = New System.Windows.Forms.Padding(5)
+        Me.lblHint.Size = New System.Drawing.Size(435, 36)
         Me.lblHint.TabIndex = 1
         Me.lblHint.Text = "This list contains patterns of season titles that should be ignored when scraping" &
     "." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Use %{season_number} to mark the location of the season number."
         '
         'pnlMain
         '
+        Me.pnlMain.AutoSize = True
         Me.pnlMain.Controls.Add(Me.tblMain)
         Me.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill
         Me.pnlMain.Location = New System.Drawing.Point(0, 0)
         Me.pnlMain.Name = "pnlMain"
-        Me.pnlMain.Size = New System.Drawing.Size(384, 460)
+        Me.pnlMain.Size = New System.Drawing.Size(484, 460)
         Me.pnlMain.TabIndex = 5
         '
         'tblMain
@@ -100,7 +102,7 @@ Partial Class frmTV_Data_SeasonTitleBlacklist
         Me.tblMain.Name = "tblMain"
         Me.tblMain.RowCount = 1
         Me.tblMain.RowStyles.Add(New System.Windows.Forms.RowStyle())
-        Me.tblMain.Size = New System.Drawing.Size(384, 460)
+        Me.tblMain.Size = New System.Drawing.Size(484, 460)
         Me.tblMain.TabIndex = 0
         '
         'dgvBlacklist
@@ -116,7 +118,7 @@ Partial Class frmTV_Data_SeasonTitleBlacklist
         Me.dgvBlacklist.Location = New System.Drawing.Point(3, 3)
         Me.dgvBlacklist.Name = "dgvBlacklist"
         Me.dgvBlacklist.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing
-        Me.dgvBlacklist.Size = New System.Drawing.Size(378, 454)
+        Me.dgvBlacklist.Size = New System.Drawing.Size(478, 454)
         Me.dgvBlacklist.TabIndex = 17
         '
         'colBlacklistText
@@ -133,7 +135,7 @@ Partial Class frmTV_Data_SeasonTitleBlacklist
         Me.pnlBottom.Dock = System.Windows.Forms.DockStyle.Bottom
         Me.pnlBottom.Location = New System.Drawing.Point(0, 460)
         Me.pnlBottom.Name = "pnlBottom"
-        Me.pnlBottom.Size = New System.Drawing.Size(384, 29)
+        Me.pnlBottom.Size = New System.Drawing.Size(484, 29)
         Me.pnlBottom.TabIndex = 6
         '
         'tblBottom
@@ -152,7 +154,7 @@ Partial Class frmTV_Data_SeasonTitleBlacklist
         Me.tblBottom.Name = "tblBottom"
         Me.tblBottom.RowCount = 1
         Me.tblBottom.RowStyles.Add(New System.Windows.Forms.RowStyle())
-        Me.tblBottom.Size = New System.Drawing.Size(384, 29)
+        Me.tblBottom.Size = New System.Drawing.Size(484, 29)
         Me.tblBottom.TabIndex = 0
         '
         'btnSetDefaults
@@ -174,7 +176,7 @@ Partial Class frmTV_Data_SeasonTitleBlacklist
         Me.btnCancel.AutoSize = True
         Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.btnCancel.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnCancel.Location = New System.Drawing.Point(306, 3)
+        Me.btnCancel.Location = New System.Drawing.Point(406, 3)
         Me.btnCancel.Name = "btnCancel"
         Me.btnCancel.Size = New System.Drawing.Size(75, 23)
         Me.btnCancel.TabIndex = 0
@@ -186,7 +188,7 @@ Partial Class frmTV_Data_SeasonTitleBlacklist
         Me.btnOK.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.btnOK.AutoSize = True
         Me.btnOK.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnOK.Location = New System.Drawing.Point(250, 3)
+        Me.btnOK.Location = New System.Drawing.Point(350, 3)
         Me.btnOK.Name = "btnOK"
         Me.btnOK.Size = New System.Drawing.Size(50, 23)
         Me.btnOK.TabIndex = 0
@@ -197,7 +199,7 @@ Partial Class frmTV_Data_SeasonTitleBlacklist
         '
         Me.StatusStrip1.Location = New System.Drawing.Point(0, 489)
         Me.StatusStrip1.Name = "StatusStrip1"
-        Me.StatusStrip1.Size = New System.Drawing.Size(384, 22)
+        Me.StatusStrip1.Size = New System.Drawing.Size(484, 22)
         Me.StatusStrip1.TabIndex = 4
         Me.StatusStrip1.Text = "StatusStrip1"
         '
@@ -205,7 +207,7 @@ Partial Class frmTV_Data_SeasonTitleBlacklist
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(384, 511)
+        Me.ClientSize = New System.Drawing.Size(484, 511)
         Me.Controls.Add(Me.pnlTop)
         Me.Controls.Add(Me.pnlMain)
         Me.Controls.Add(Me.pnlBottom)
@@ -214,6 +216,7 @@ Partial Class frmTV_Data_SeasonTitleBlacklist
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(500, 550)
         Me.Name = "frmTV_Data_SeasonTitleBlacklist"
         Me.Text = "Season Title Blacklist"
         Me.pnlTop.ResumeLayout(False)
